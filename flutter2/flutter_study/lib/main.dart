7dd59fa9a0a9fd8944df6d79cb7d76543bb63d73
zad <1042480866@qq.com>
2021-03-11 22:22:51 +0800

    手势和指针

--- flutter2/flutter_study/lib/main.dart
+++ flutter2/flutter_study/lib/main.dart
@@ -1,73 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/f09-state-manager/viewmodel/counter_view_model.dart';
-import 'package:flutter_study/f09-state-manager/viewmodel/initialize_providers.dart';
-import 'package:flutter_study/f09-state-manager/viewmodel/user_view_model.dart';
-import 'package:provider/provider.dart';
 
-main() {
-  runApp(MultiProvider(
-    providers: providers,
-    child: MyApp(),
-  ));
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ADHomePage(),
-    );
-  }
-}
-
-class ADHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    print("_ADHomePageState - build");
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Provider"),
-      ),
-      floatingActionButton: Selector<CounterViewModel, CounterViewModel>(
-        builder: (context, counterVM, child) {
-          print("_ADHomePageState - Selector - builder");
-          return FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              counterVM.counter++;
-            },
-          );
-        },
-        selector: (ctx, counterVM) => counterVM,
-        shouldRebuild: (prev, next) => false,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ADShowData01(),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("手势Gesture"),
+        ),
+        body: Center(
+          child: GestureDemo02(),
         ),
       ),
     );
   }
 }
 
-class ADShowData01 extends StatelessWidget {
+class GestureDemo02 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print("ADShowData02 - build");
-    return Card(
-      child: Consumer2<CounterViewModel, UserInfoViewModel>(
-        builder: (context, counterVM, userInfoVM, child) {
-          print("ADShowData02- Consumer - builder");
-          return Text(
-            "counter: ${counterVM.counter} name: ${userInfoVM.userInfo.name}",
-            style: TextStyle(fontSize: 30),
-          );
-        },
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        GestureDetector(
+          onTapDown: (details) {
+            print("outer click");
+          },
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.yellow,
+            alignment: Alignment.center,
+          ),
+        ),
+        IgnorePointer(
+          child: GestureDetector(
+            onTapDown: (details) {
+              print("inner click");
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class GestureDemo01 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (details) {
+        print("outer click");
+      },
+      child: Container(
+        width: 200,
+        height: 200,
+        color: Colors.yellow,
+        alignment: Alignment.center,
+        child: GestureDetector(
+          onTapDown: (details) {
+            print("inner click");
+          },
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+        ),
       ),
-      color: Colors.green,
+    );
+
+    Stack(
+      alignment: Alignment.center,
+      children: <Widget>[IgnorePointer()],
     );
   }
 }
